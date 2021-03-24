9e16b521b4a60d1fb88095584adec5727e03ff70
imihajlovic98 <ivan.mihajlovic-milin@pm.me>
2021-03-24 16:28:12 +0100

    Add gtest (currently missing gtest/gtest.h)

---
+++ test/TestLaplacian.cpp
@@ -0,0 +1,13 @@
+// File       : TestLaplacian.cpp
+// Created    : Wed Mar 24 2021  4:16:12 pm CET (+0100)
+// Author     : Ivan Mihajlovic Milin
+// Description: Unit test for Laplacian kernels
+// Copyright 2021 ETH Zurich. All Rights Reserved.
+
+// #include <gtest/gtest.h>
+#include <cstdio>
+
+int main() 
+{
+    printf("Hey at least it compiles.\n"); 
+}
