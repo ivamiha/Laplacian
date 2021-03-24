b12ee71ff9cecc1f3c4332f9e1e57859c49667fa
imihajlovic98 <ivan.mihajlovic-milin@pm.me>
2021-03-24 11:48:01 +0100

    Adding discretization switch flags

--- src/main.cpp
+++ src/main.cpp
@@ -9,38 +9,39 @@
 #include "Cubism/Mesh/StructuredUniform.h"
 #include "Cubism/Block/Field.h"
 #include "Cubism/Common.h"
-#include "Cubism/Compiler.h"
 
 #include "Laplacian2f.h"
 #include "Laplacian2s.h"
 #include "Laplacian4f.h"
 #include "Laplacian4s.h"
+
 #include <iostream>
+#include <assert.h>
+
+// enable 4th-order CDS, default 2nd-order CDS 
+#define _ACCUR_ 
+// enable benchmarking, default no benchmarking
+// #define _BENCH_  
 
 using namespace Cubism;
 
 int main() 
 {    
-    // welcome user & request input for simulation configurations
-    int method;             // user defined sim configuration 
+    // welcome & inform user what program configuration is running
     printf("\n------------------------------------------------------------\n");
     printf("T E S T   L A P L A C I A N   C O M P U T E   K E R N E L\n\n");
     printf("Optimized Laplacian compute kernel utilizing CubismNova\n");
     printf("============================================================\n");
-    printf("Selection menu:\n");
-    printf("1) benchmark 2nd-order CDS implementation;\n");
-    printf("2) benchmark 4th-order CDS implementation;\n");
-    printf("3) run OVS for 2nd-order CDS implementation;\n");
-    printf("4) run OVS for 4th-order CDS implementation.\n");
-    printf("Please input desired simulation configuration:\n");
-    std::cin >> method; 
-    // ensure that selected method is indeed an integer in program menu
-    method = round(method);
-    while (method < 1 || method > 4) {
-        printf("Please ensure input number is correct:\n");
-        std::cin >> method;
-        method = round(method);
-    }
+#ifdef _BENCH_
+    printf("Benchmarking "); 
+#else
+    printf("Running "); 
+#endif /* BENCH */
+#ifdef _ACCUR_ 
+    printf("4th-order CDS implementation.\n"); 
+#else
+    printf("2nd-order CDS implementation.\n"); 
+#endif /* ACCUR */   
     
     // initialize simulation variables to be used throughout
     double time = 50.0;     // simulation duration [s]
@@ -59,7 +60,7 @@ int main()
     // define number of blocks & cells per block
     const MIndex nblocks(8);                        // 8^3 = 512 blocks
     const MIndex block_cells(16);                   // 16^3 = 4096 cells/block 
-    // map the blocks & cells onto scalar grids on default domain [0,1]^3 [m] 
+    // map the blocks & cells on domain [0,1]^3 [m] 
     SGrid grid(nblocks, block_cells);               // solution storage
     SGrid temp(nblocks, block_cells);               // temporary storage
     const Point h = grid.getMesh().getCellSize(0);  // grid spacing [m]
@@ -81,20 +82,25 @@ int main()
         fIC(*f); 
     }
 
-    // setup lab
-    // TODO: define s4 & implement user "choice" in an intelligent way 
+    // define numerical parameters based on chosen discretization
+#ifdef _ACCUR_                      
+    const Stencil s(-2, 3, true);               // 4th-order CDS stencil 
+    double dt = (h[0] * h[0]) / (2 * D);        // TODO: 4th-order stability
+    double fac = (D * dt) / (12 * h[0] * h[0]); // 4th-order Laplacian factor
+#else                                          
+    const Stencil s(-1, 2, false);              // 2nd-order CDS stencil
+    double dt = (h[0] * h[0]) / (2 * D);        // 2nd-order stability 
+    double fac = (D * dt) / (h[0] * h[0]);      // 2nd-order Laplacian factor 
+#endif /* _ACCUR_ */
+
+    // setup lab 
     DataLab dlab; 
-    const Stencil s2(-1, 2, false);
-    dlab.allocate(s2, grid[0].getIndexRange()); 
+    dlab.allocate(s, grid[0].getIndexRange()); 
 
     // get block field index functor for periodic block accessing
     auto findex = grid.getIndexFunctor(0); 
 
-    // compute simulation constants for 2nd-order CDS stability conditions
-    // TODO: add 4th order stability conditions
-    double dt = (h[0] * h[0]) / (2 * D);
-    double fac = (D * dt) / (h[0] * h[0]); 
-
+#ifdef _ACCUR_ 
     // loop through time 
     for (double t = 0.0; t < time; t += dt) 
     {
@@ -106,13 +112,31 @@ int main()
             dlab.loadData(bf.getState().block_index, findex);  
             auto &tf = temp[bf.getState().block_index];
 
-            // apply Laplacian discretization 
-            // TODO: select intelligently which function is applied
-            Laplacian2f(dlab,tf);
+            // apply 4th-order central Laplacian discretization 
+            Laplacian4f(dlab,tf);
         }
 
         // TODO: pass current solution into datalab
-    } 
+    }
+#else 
+    // loop through time 
+    for (double t = 0.0; t < time; t += dt) 
+    { 
+        // loop through blocks in the grid
+        for (auto f : grid) 
+        {
+            // reference fields & load data into dlab object for current block
+            const FieldType &bf = *f;  
+            dlab.loadData(bf.getState().block_index, findex);  
+            auto &tf = temp[bf.getState().block_index];
+                                                                             
+            // apply 2nd-order central Laplacian discretization 
+            Laplacian2f(dlab,tf);
+        }
+                                                                             
+        // TODO: pass current solution into datalab
+    }
+#endif /* _ACCUR_ */
 
     return 0; 
 }
