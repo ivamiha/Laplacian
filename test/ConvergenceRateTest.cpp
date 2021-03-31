3bebd6f08591d9f8645cdc29d7950e903614fa53
imihajlovic98 <ivan.mihajlovic-milin@pm.me>
2021-03-31 10:13:35 +0200

    Deleting old tests, adding new

---
+++ test/ConvergenceRateTest.cpp
@@ -0,0 +1,16 @@
+// File       : ConvergenceRateTest.cpp
+// Created    : Wed Mar 31 2021 10:08:03 am CEST (+0200)
+// Author     : Ivan Mihajlovic Milin
+// Description: Description
+// Copyright 2021 ETH Zurich. All Rights Reserved.
+
+#include "Cubism/BC/Dirichlet.h"
+#include "gtest/gtest.h"
+#include <cstdio>
+
+int main()
+{
+    printf("Testing linking... works!\n"); 
+
+    return 0; 
+}
