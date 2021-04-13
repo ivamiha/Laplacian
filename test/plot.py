ca8ecbd0dbdcbb9580611874fb5004a88d506e0b
imihajlovic98 <ivan.mihajlovic-milin@pm.me>
2021-04-13 11:57:28 +0200

    Adding minor formatting fixes

--- test/plot.py
+++ test/plot.py
@@ -1,6 +1,9 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
+plt.rcParams['mathtext.fontset'] = 'cm'
+plt.rcParams['font.family'] = 'STIXGeneral'
+
 data = np.loadtxt('../build/test/ooa.txt')
 
 x = data[:,0]
@@ -8,12 +11,11 @@ y = data[:,1]
 
 fig, ax = plt.subplots()
 
-ax.plot(x,y,'bo-',label='actual OOA')
+ax.plot(x,y,'ko-',label='actual OOA')
 ax.axhline(y=2.0, color = 'k', linestyle='dotted', label='nominal OOA')
-ax.set_xlabel('$log(h_j)$')
-ax.set_ylabel('$Δ log(E_j)/Δ log(h_j)$')
+ax.set_xlabel('log($h_j$)')
+ax.set_ylabel('Δlog($E_j$) / Δlog($h_j$)')
 ax.set_title('Code verfication - actual order of accuracy')
 ax.legend(loc='lower left')
 plt.ylim(-1.0, plt.ylim()[1] + 0.5)
-plt.grid(linestyle='dotted')
 plt.savefig('../build/test/OVS.png', dpi=300)
