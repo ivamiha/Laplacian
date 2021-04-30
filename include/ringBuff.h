00c617f3fdc7c5adc4fa5f8134c452ed4570cb09
Ivan <ivan@pop-os.localdomain>
2021-04-30 08:55:02 +0200

    Adding ringBuff modifications & optimizations

--- include/ringBuff.h
+++ include/ringBuff.h
@@ -4,78 +4,55 @@
 // Description: Bare-bone ring buffer implementation
 // Copyright 2021 ETH Zurich. All Rights Reserved.
 
-#include <cstdlib>
-#include <cstddef>
-
 /** 
- * @brief Ring buffer (circular buffer) structure template
- * @param head Current head position (write pointer)
- * @param tail Current tail position (read pointer)
- * @param capacity Maximum size of the ringBuff (array size)
- * @param data Pointer to an array where ringBuff data is stored
+ * @brief Ring buffer (circular buffer) struct
  *
- * @rst Bare-bone implementation of a ring buffer. Several "safety" features 
- * commonly included (e.g. checking for NULL full buffer) are ommitted, since
- * they require if statements which slow down execution. Only Laplacian kernels
- * utilize the ring buffer, such that the user is never exposed to them.  
+ * @rst Bare-bone implementation of a ring buffer. Several safety features 
+ * which are commonly used (e.g. checking for NULL or full buffer) are 
+ * ommitted. User never interacts with the struct, as only Laplacian kernels
+ * access it -- therefore, this approach reduces struct memory footprint 
+ * and speeds-up execution due to lack of if statements. 
  * @endrst 
  * */
-template <typename DataType>
+// TODO: [imihajlovic@student.ethz.ch; Tue Apr 27 2021 04:30:02 PM (+0200)] 
+// currently supports only double-precision, figure out how to extend to float
 struct ringBuff {
-    size_t head;        
-    size_t tail;        
-    size_t capacity;    
-    DataType* data;     
+    size_t head;        // current writing position    
+    size_t capacity;    // maximum size of created ring buffer
+    double **data;      // pointer to array of pointers to doubles
 }; 
 
-// create global definition of ringBuff_t
-typedef struct ringBuff<double> ringBuff_t; 
+typedef struct ringBuff ringBuff_t; 
 
 /** 
  * @brief Allocate memory & initialize ring buffer of certain capacity
  * @param _capacity Capacity (maximum size) of ring buffer to be created 
  * */
 ringBuff_t* ringCreate(size_t _capacity) {
-    // allocate memory of size of the ringBuff struct
-    ringBuff_t* ring = (ringBuff_t*)malloc(sizeof(ringBuff_t)); 
+    ringBuff_t *ring = new ringBuff_t; 
 
-    // initialize member varaibles of the ringBuff struct
-    ring -> tail = 0; 
     ring -> head = 0; 
     ring -> capacity = _capacity; 
-    ring -> data = (double*)malloc(_capacity * sizeof(double));
+    ring -> data = new double*[_capacity];
 
     return ring; 
 }
 
 /** 
  * @brief Enqueue/write item into buffer & advance head pointer 
- * @param ring Pointer to the created ringBuff_t structure
- * @param item Data to be written to the buffer 
+ * @param ring Pointer to the created ringBuff_t struct
+ * @param item Pointer to item which will be written to the buffer
  * */
-void ringEnqueue(ringBuff_t* ring, double item) {
-    // add item to buffer
+void ringEnqueue(ringBuff_t *ring, double *item) {
     ring -> data[ring -> head] = item; 
-    // advance head
     ring -> head = (ring -> head + 1) % ring -> capacity;
 }
 
 /** 
- * @brief Advance tail pointer to dequeue (forget) item first added to buffer
- * @param ring Pointer to the created ringBuff_t structure 
- * */
-void ringDequeue(ringBuff_t* ring) {
-    // advance tail
-    ring -> tail = (ring -> tail + 1) % ring -> capacity; 
-}
-
-/** 
  * @brief Free memory that was dynamically allocated in ring buffer creation
- * @param ring Pointer to the created ringBuff_t structure
+ * @param ring Pointer to the created ringBuff_t struct
  * */
 void ringFree(ringBuff_t* ring) {
-    // free the dynamically allocated memory for the data array
-    free(ring -> data); 
-    // free the dynamically allocated memory for the ringBuff struct
-    free(ring); 
+    delete[] ring -> data; 
+    delete ring;  
 }
